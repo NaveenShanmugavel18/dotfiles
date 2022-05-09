5563e82396a87597e160f5e0d12beb4f5d0caca8
Naveen Shanmugavel <naveensv18@gmail.com>
2022-05-09 10:01:20 +0530

    added brew commands

---
+++ install.sh
@@ -0,0 +1,18 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask brave-browser
+brew install --cask ticktick
+brew install --cask clocker
+brew install --cask slack
+brew install --cask discord
+brew install --cask zoom
+brew install --cask postman
+brew install --cask telegram-desktop
+brew install docker
+brew install --cask teamviewer
+brew install --cask anydesk
+brew install --cask iterm2
+brew install --cask microsoft-teams
+
+git config --global user.name "Naveen Shanmugavel"
+git config --global user.email naveensv18@gmail.com
