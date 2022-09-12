e8783a1564f34aa3ebb5a73ace948623b621314f
Naveen Shanmugavel <naveensv18@gmail.com>
2022-09-12 16:42:43 +0530

    added raindrop bookmark manager

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ brew install --cask vlc
 brew install --cask whatsapp
 brew install --cask signal
 brew install --cask flameshot
+brew install --cask raindropio
 
 git config --global user.name "Naveen Shanmugavel"
 git config --global user.email naveensv18@gmail.com
