dbb416e557ab005f17926f152f84b6d83c84a3cd
Naveen Shanmugavel <naveensv18@gmail.com>
2024-08-25 14:07:17 +0530

    added pdfsam basic merge and split

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+# Install homebrew, iterm2, zsh, ohmyzsh and tweak plugin
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask iterm2
+brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Add plugin for zsh
 
 brew install --cask brave-browser
 brew install --cask ticktick
@@ -10,7 +15,6 @@ brew install --cask postman
 brew install --cask telegram-desktop
 brew install --cask teamviewer
 brew install --cask anydesk
-brew install --cask iterm2
 brew install --cask microsoft-teams
 brew install --cask vlc
 brew install --cask whatsapp
@@ -19,11 +23,16 @@ brew install --cask flameshot
 brew install --cask raindropio
 brew install --cask rectangle
 brew install --cask mysqlworkbench
+brew install --cask pdfsam-basic
 brew install texlive
 brew install redis
 brew install mysql
 brew install docker
 brew install gh
+
+
+# install go and set variables to ~/.zshrc
+brew install go
 
 git config --global user.name "Naveen Shanmugavel"
 git config --global user.email naveensv18@gmail.com
